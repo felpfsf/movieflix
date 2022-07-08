7ae1981acc2d41058b9c455c5d88f72a777ac51c
felpfsf <feferreira.sf@gmail.com>
2022-07-08 20:40:58 -0300

    add MoviesRow component to be rendered

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
 import Main from '../components/Main'
+import MoviesRow from '../components/MoviesRow'
+import requests from '../Requests'
 
 const Home = () => {
   return (
     <>
       <Main />
+      <MoviesRow rowID='upComing' title='Up Coming' fetchURL={requests.requestUpcomming} />
+      <MoviesRow rowID='topMovies' title='Top Movies' fetchURL={requests.requestTop} />
+      <MoviesRow rowID='trending' title='Trending Now' fetchURL={requests.requestTrend} />
+      <MoviesRow rowID='pop' title='Most Populars' fetchURL={requests.requestPop} />
+      <MoviesRow rowID='action' title='Action Movies' fetchURL={requests.requestAction} />
     </>
   )
 }
