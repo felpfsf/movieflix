ba93cbf9a1ebd00ecc825bcdfda3a1fe980c2ac4
felpfsf <feferreira.sf@gmail.com>
2022-07-08 20:42:36 -0300

    changed api key to env variable

--- src/Requests.js
+++ src/Requests.js
@@ -1,10 +1,10 @@
-const key = '692e267ea5d292931da8020a01bb116c'
+const key = import.meta.env.VITE_TMDB_KEY
 
 const requests = {
-  requestPop: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
-  requestTop: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
-  requestTrend: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
-  requestUpcomming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  requestPop: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1&include_adult=false`,
+  requestTop: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1&include_adult=false`,
+  requestTrend: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2&include_adult=false`,
+  requestUpcomming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1&include_adult=false`,
   requestAction: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=action&page=1&include_adult=false`
 }
 
