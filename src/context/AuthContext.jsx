64b90490719eb7f6dd442c5b87e052139be5e8db
felpfsf <feferreira.sf@gmail.com>
2022-07-09 16:18:01 -0300

    connecting to firebase

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,45 @@
+import { createContext, useContext, useState, useEffect } from 'react'
+import { auth } from '../firebaseConfig'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from 'firebase/auth'
+
+const AuthContext = createContext()
+
+export function AuthContextProvider({ children }) {
+  const [user, setUser] = useState({})
+
+  function signUp(email, password) {
+    return createUserWithEmailAndPassword(auth, email, password)
+  }
+
+  function logIn(email, password) {
+    return signInWithEmailAndPassword(auth, email, password)
+  }
+
+  function logOut() {
+    return signOut(auth)
+  }
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, currentUser => {
+      setUser(currentUser)
+    })
+    return () => {
+      unSubscribe()
+    }
+  })
+
+  return (
+    <AuthContext.Provider value={{ signUp, logIn, logOut, user }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export function UserAuth() {
+  return useContext(AuthContext)
+}
